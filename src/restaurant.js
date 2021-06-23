3b2617bb9a75c2962cc424fcda25ba3e9f4c4174
MilenaCLeme <milenaleme4@hotmail.com>
2021-06-23 13:30:01 -0300

    Requisitos 9 finalizado arquivo src

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,6 +79,28 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, 
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = (objetoPassadoPorParametro) => ({
+  fetchMenu: () => objetoPassadoPorParametro,
+  consumption: [],
+  order: function order(parametro1) {
+    let consumo = this.consumption; 
+    consumo.push(parametro1);
+  },
+  pay: function pay() {
+    let soma = 0;
+    for (let index = 0; index < this.consumption.length; index += 1) {
+      let contem = this.consumption[index];
+      if (objetoPassadoPorParametro.food[contem] !== undefined) {
+        soma += objetoPassadoPorParametro.food[contem];
+      }
+      if (objetoPassadoPorParametro.drinks[contem] !== undefined) {
+        soma += objetoPassadoPorParametro.drinks[contem];
+      }
+    }
+    let acrescidos = soma * 0.1;
+    let somaFinal = acrescidos + soma;
+    return somaFinal;
+  },
+});
 
 module.exports = createMenu;
