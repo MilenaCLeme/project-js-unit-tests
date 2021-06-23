50047b99c073ee5a7cea04c77ec55a52ddafecf0
MilenaCLeme <milenaleme4@hotmail.com>
2021-06-23 13:28:59 -0300

    Requisito 5 finalizado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (item1) => ({
+  name: item1,
+  feedback: () => 'Eita pessoa boa!', 
+});
 
 module.exports = createStudent;
