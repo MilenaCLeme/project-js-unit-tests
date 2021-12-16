de2f736bd6fb7bd5eacac39b082e1b35f90f65a1
MilenaCLeme <milenaleme4@hotmail.com>
2021-12-15 22:36:42 -0300

    finalizado

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -42,7 +42,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     parseFloat((0.2 + 0.1).toPrecision(2))
     const test = circle(3).area
     const testArea = test.toPrecision(2)
-    assert.strictEqual(testArea, 28)
+    assert.strictEqual(testArea, '28')
 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 })
